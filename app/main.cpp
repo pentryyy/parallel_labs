082b2a45877c62ab845eba65205f9e85b38f3b7d
pentryyy <m.v.volikov@gmail.com>
2024-10-03 22:14:39 +0300

    first commit

---
+++ app/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>               // подключаем заголовочный файл iostream
+ 
+int main()                          // определяем функцию main
+{                                   // начало функции
+    std::cout << "Hello METANIT.COM!";     // выводим строку на консоль
+    return 0;                       // выходим из функции
+}        
